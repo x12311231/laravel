f75880997240d4f460ac05587797e3faa97353ee
ubuntu22.04LTS <xhqpro@outlook.com>
2023-06-13 17:59:47 +0800

    ...

---
+++ Envoyfd20837d9f47ff4d4c3b7f1c46e74d8a.php
@@ -0,0 +1,33 @@
+<?php $__container->servers(['web' => 'root@192.168.0.19', 'web1' => 'root@192.168.0.18]); ?>
+
+@task('deploy')
+    cd /tmp
+    mkdir laravel
+    cd laravel
+    echo hello > hello
+@endtask
+
+@task('setup')
+    server-update
+    setup-tools
+    setup-docker
+@endtask
+
+@task('server-update')
+    sudo yum install -y epel-release
+@endtask
+
+@task('setup-tools')
+    sudo yum install -y vim
+@endtask
+
+@task('setup-docker')
+    sudo yum install -y yum-utils \
+    device-mapper-persistent-data \
+    lvm2
+    sudo yum-config-manager \
+    --add-repo \
+    https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/centos/docker-ce.repo
+    sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
+@endtask
