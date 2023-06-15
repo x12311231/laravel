813fe2e41a2824001fb09e27991d46364e57d03a
ubuntu22.04LTS <xhqpro@outlook.com>
2023-06-15 17:38:16 +0800

    ...

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/token', function (Request $request) {
+//    return 'ok';
+    $validated = \Illuminate\Support\Facades\Validator::make($request->all(), [
+        'email' => ['required', 'email'],
+        'password' => ['required']
+    ])->validated();
+    $user = \App\Models\User::where(['email' => $validated['email']])
+        ->firstOrFail();
+    \Illuminate\Support\Facades\Auth::setUser($user);
+    if (!\Illuminate\Support\Facades\Auth::validate($validated)) {
+        throw new \Illuminate\Auth\AuthenticationException();
+    }
+    return $user->createToken('user_' . $user->email);
+});
