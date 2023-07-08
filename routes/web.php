16a0d81528790060e80af345272355400b57ce37
ubuntu22.04LTS <xhqpro@outlook.com>
2023-07-08 09:22:37 +0800

    ...

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/precognition', function () {
+    return 'hello precognition';
+})->middleware([\Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class]);
