29f26e0936e70db7099b37aabe2d2c561de0c213
Your Name <you@example.com>
2023-08-03 16:41:50 +0800

    ...

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Browser;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTruncation;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
@@ -14,21 +17,26 @@ class LoginTest extends DuskTestCase
 
     /**
      * A basic browser test example.
+     * @throws \Throwable
      */
-//    public function test_basic_example(): void
-//    {
-//        $user = User::factory()->create([
-//            'email' => 'taylor@laravel.com',
-//        ]);
-//
-//        $this->browse(function (Browser $browser) use ($user) {
-//            $browser->visit('/login')
-//                ->type('email', $user->email)
-//                ->type('password', 'password')
-//                ->press('Login')
-//                ->assertPathIs('/home');
-//        });
-//    }
+    public function test_basic_example(): void
+    {
+        $password = 'password';
+        $user = User::factory()->create([
+            'email' => 'taylor@laravel.com',
+            'password' => Hash::make($password)
+        ]);
+
+        $this->assertDatabaseCount(User::class, 1);
+        $this->browse(function (Browser $browser) use ($user, $password) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', $password)
+//                ->press('Log in')
+                    ->click('#Login')
+                ->assertPathIs('/dashboard');
+        });
+    }
     /**
      * A Dusk test example.
      */
