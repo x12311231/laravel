e9b9ea366f3d84031ea80f5be2d5ac601e634d09
Your Name <you@example.com>
2023-09-07 17:56:20 +0800

    ...

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,4 +1,4 @@
-@servers(['web' => 'root@192.168.0.19', 'web1' => 'root@192.168.0.18'])
+@servers(['web' => 'root@192.168.0.19', 'web1' => 'root@117.72.10.250', 'web2' => 'root@45.32.33.65', 'web3' => 'docker1@192.168.0.19', 'web4' => 'root@202.182.116.24'])
 
 @task('deploy')
     cd /tmp
@@ -7,10 +7,18 @@
     echo hello > hello
 @endtask
 
-@story('setup', ['parallel' => true])
+@story('setup', ['parallel' => true, 'on' => ['web4']])
     timezone
-    server-update
     setup-tools
+    remove-docker
+    setup-docker
+    docker-registry
+    setup-dnmp
+    setup-mycms
+@endstory
+
+@story('reinstall-docker', ['parallel' => true, 'on' => ['web4']])
+    remove-docker
     setup-docker
 @endstory
 
@@ -19,21 +27,36 @@
 @endtask
 
 @task('setup-tools')
-    sudo yum install -y vim
+    sudo yum install -y vim htop git
 @endtask
 
 @task('timezone')
     timedatectl set-timezone Asia/Shanghai
 @endtask
+
+@task('remove-docker')
+    sudo yum remove -y docker \
+    docker-client \
+    docker-client-latest \
+    docker-common \
+    docker-latest \
+    docker-latest-logrotate \
+    docker-logrotate \
+    docker-engine \
+    docker-compose
+@endtask
+
 @task('setup-docker')
     sudo yum install -y yum-utils \
     device-mapper-persistent-data \
     lvm2
     sudo yum-config-manager \
     --add-repo \
-    https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/centos/docker-ce.repo
+    https://download.docker.com/linux/centos/docker-ce.repo
     sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-    [ ! -f /usr/local/bin/docker-compose ] && ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
+    ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
+    sudo systemctl start docker
+    sudo systemctl enable docker
 @endtask
 
 @task('confirm', ['confirm' => true])
@@ -48,3 +71,35 @@
     echo web1 confirm `date` >> /tmp/confirm.log
 @endtask
 
+@task('docker-registry')
+    sudo mkdir -p /etc/docker
+    sudo tee /etc/docker/daemon.json <<-'EOF'
+    {
+      "registry-mirrors": ["https://registry.docker-cn.com"]
+    }
+    EOF
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
+@endtask
+
+@task('setup-dnmp')
+    mkdir -p /website/
+    cd /website/
+    git clone https://gitee.com/xhq192/dnmp.git
+    cd dnmp
+    git fetch origin php73_80-mysql57-redis5-nginx
+    git checkout php73_80-mysql57-redis5-nginx
+    cp env.demo .env
+    sed -i 's/MYSQL5_ROOT_PASSWORD=123456/MYSQL5_ROOT_PASSWORD=docker123456/g' .env
+    sudo service docker start
+    docker-compose up -d
+@endtask
+
+@task('setup-mycms')
+    cd /website/dnmp
+    cd www
+    git clone https://gitee.com/qq386654667/mycms.git
+    cd ../services/nginx/conf.d
+    cp laravel.conf mycms.conf
+    sed -i 's/laravel/mycms/g' mycms.conf
+@endtask
